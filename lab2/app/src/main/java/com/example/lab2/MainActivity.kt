4bf2e14a67effcb227a8a05c894978822640c5bb
nika-nii <farakshin.nikita@gmail.com>
2021-03-10 19:03:36 +0300

    Data

--- lab2/app/src/main/java/com/example/lab2/MainActivity.kt
+++ lab2/app/src/main/java/com/example/lab2/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.lab2
 
+import android.content.res.Resources
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
 import android.view.ViewGroup
@@ -7,30 +9,40 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import android.view.LayoutInflater
+import android.widget.ImageView
 import android.widget.TextView
 
-class CustomRecyclerAdapter(private val values: List<String>) :
+class Question(val question: String, val answers: List<String>, val avatar: Drawable)
+
+class CustomRecyclerAdapter(private val values: List<Question>) :
     RecyclerView.Adapter<CustomRecyclerAdapter.MyViewHolder>() {
 
     override fun getItemCount() = values.size
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val itemView = LayoutInflater.from(parent?.context).inflate(R.layout.recyclerview_item, parent, false)
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_item, parent, false)
         return MyViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.largeTextView?.text = values[position]
-        holder.smallTextView?.text = "кот"
+        val question = values[position]
+        holder.questionText?.text = question.question
+        holder.firstAnswerText?.text = question.answers[0]
+        holder.answersCountText?.text = question.answers.size.toString()
+        holder.avatarView?.setImageDrawable(question.avatar)
     }
 
     class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var largeTextView: TextView? = null
-        var smallTextView: TextView? = null
+        var questionText: TextView? = null
+        var firstAnswerText: TextView? = null
+        var answersCountText: TextView? = null
+        var avatarView: ImageView? = null
 
         init {
-            largeTextView = itemView?.findViewById(R.id.textViewLarge)
-            smallTextView = itemView?.findViewById(R.id.textViewSmall)
+            questionText = itemView.findViewById(R.id.questionText)
+            firstAnswerText = itemView.findViewById(R.id.firstAnswerText)
+            answersCountText = itemView.findViewById(R.id.answersCountText)
+            avatarView = itemView.findViewById(R.id.avatarView)
         }
     }
 }
@@ -41,13 +53,30 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val recyclerView = findViewById<View>(R.id.recyclerView) as RecyclerView
         recyclerView.layoutManager = LinearLayoutManager(this)
-
         recyclerView.adapter = CustomRecyclerAdapter(fillList())
     }
 
-    private fun fillList(): List<String> {
-        val data = mutableListOf<String>()
-        (0..30).forEach { i -> data.add("$i element") }
+    private fun fillList(): List<Question> {
+        val data = mutableListOf<Question>()
+        val res: Resources = resources
+        val questions = res.getStringArray(R.array.questions)
+        val size = questions.size
+        for (i in 0 until size){
+            val answersName = "answer${(i+1).toString().padStart(2, '0')}"
+            val avatarName = "avatar${(i+1).toString().padStart(2, '0')}"
+            val answers: List<String> = res.getStringArray(
+                            res.getIdentifier(answersName, "array", packageName)
+            ).asList()
+            data.add(
+                    Question(
+                            questions[i],
+                            answers,
+                            res.getDrawable(
+                                    res.getIdentifier(avatarName, "drawable", packageName), theme
+                            )
+                    )
+            )
+        }
         return data
     }
 }
