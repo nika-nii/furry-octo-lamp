09699b3cb83fa571536a4458a32f3c506113b654
nika-nii <farakshin.nikita@gmail.com>
2021-03-11 19:35:27 +0300

    Фрагменты ворочаются

---
+++ lab2/app/src/main/java/com/example/lab2/ListFragment.kt
@@ -0,0 +1,92 @@
+package com.example.lab2
+
+import android.content.Intent
+import android.content.res.Resources
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+
+class Question(val question: String, val answers: List<String>, val avatar: Drawable)
+
+class CustomRecyclerAdapter(private val values: List<Question>, private val onClickCallback: (Int) -> Unit ) :
+        RecyclerView.Adapter<CustomRecyclerAdapter.MyViewHolder>() {
+
+    override fun getItemCount() = values.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_item, parent, false)
+        return MyViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val question = values[position]
+        holder.questionText?.text = question.question
+        holder.firstAnswerText?.text = question.answers[0]
+        holder.answersCountText?.text = question.answers.size.toString()
+        holder.avatarView?.setImageDrawable(question.avatar)
+        holder.itemView.setOnClickListener{
+            onClickCallback(position)
+        }
+    }
+
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var questionText: TextView? = null
+        var firstAnswerText: TextView? = null
+        var answersCountText: TextView? = null
+        var avatarView: ImageView? = null
+
+        init {
+            questionText = itemView.findViewById(R.id.questionText)
+            firstAnswerText = itemView.findViewById(R.id.firstAnswerText)
+            answersCountText = itemView.findViewById(R.id.answersCountText)
+            avatarView = itemView.findViewById(R.id.avatarView)
+        }
+    }
+}
+
+class ListFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        val rootView = inflater.inflate(R.layout.fragment_list, container, false)
+        val recyclerView = rootView?.findViewById<View>(R.id.recyclerView) as RecyclerView
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = CustomRecyclerAdapter(fillList()){ questionId ->
+            val intent = Intent(activity, MainActivity::class.java)
+            intent.putExtra("questionId", questionId)
+            startActivity(intent)
+        }
+        return rootView
+    }
+
+    private fun fillList(): List<Question> {
+        val data = mutableListOf<Question>()
+        val res: Resources = resources
+        val questions = res.getStringArray(R.array.questions)
+        val size = questions.size
+        for (i in 0 until size) {
+            val answersName = "answer${(i + 1).toString().padStart(2, '0')}"
+            val avatarName = "avatar${(i + 1).toString().padStart(2, '0')}"
+            val answers: List<String> = res.getStringArray(
+                    res.getIdentifier(answersName, "array", activity?.packageName)
+            ).asList()
+            data.add(
+                    Question(
+                            questions[i],
+                            answers,
+                            res.getDrawable(
+                                    res.getIdentifier(avatarName, "drawable", activity?.packageName), activity?.theme
+                            )
+                    )
+            )
+        }
+        return data
+    }
+}
--- lab2/app/src/main/java/com/example/lab2/MainActivity.kt
+++ lab2/app/src/main/java/com/example/lab2/MainActivity.kt
@@ -1,82 +1,28 @@
 package com.example.lab2
 
-import android.content.res.Resources
-import android.graphics.drawable.Drawable
+import android.app.Fragment
 import android.os.Bundle
-import android.view.View
-import android.view.ViewGroup
+import android.os.PersistableBundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
-import android.view.LayoutInflater
-import android.widget.ImageView
-import android.widget.TextView
+import androidx.fragment.app.commit
 
-class Question(val question: String, val answers: List<String>, val avatar: Drawable)
 
-class CustomRecyclerAdapter(private val values: List<Question>) :
-    RecyclerView.Adapter<CustomRecyclerAdapter.MyViewHolder>() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
-    override fun getItemCount() = values.size
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        supportFragmentManager.commit {
+            setReorderingAllowed(false)
+            add(R.id.fragment_container_view, DataFragment())
+        }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.recyclerview_item, parent, false)
-        return MyViewHolder(itemView)
     }
 
-    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val question = values[position]
-        holder.questionText?.text = question.question
-        holder.firstAnswerText?.text = question.answers[0]
-        holder.answersCountText?.text = question.answers.size.toString()
-        holder.avatarView?.setImageDrawable(question.avatar)
-    }
-
-    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        var questionText: TextView? = null
-        var firstAnswerText: TextView? = null
-        var answersCountText: TextView? = null
-        var avatarView: ImageView? = null
-
-        init {
-            questionText = itemView.findViewById(R.id.questionText)
-            firstAnswerText = itemView.findViewById(R.id.firstAnswerText)
-            answersCountText = itemView.findViewById(R.id.answersCountText)
-            avatarView = itemView.findViewById(R.id.avatarView)
+    override fun onAttachFragment(fragment: Fragment) {
+        super.onAttachFragment(fragment)
+        if (fragment.id == R.id.fragment_data) {
+            fragment.arguments = intent.extras
         }
     }
-}
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val recyclerView = findViewById<View>(R.id.recyclerView) as RecyclerView
-        recyclerView.layoutManager = LinearLayoutManager(this)
-        recyclerView.adapter = CustomRecyclerAdapter(fillList())
-    }
-
-    private fun fillList(): List<Question> {
-        val data = mutableListOf<Question>()
-        val res: Resources = resources
-        val questions = res.getStringArray(R.array.questions)
-        val size = questions.size
-        for (i in 0 until size){
-            val answersName = "answer${(i+1).toString().padStart(2, '0')}"
-            val avatarName = "avatar${(i+1).toString().padStart(2, '0')}"
-            val answers: List<String> = res.getStringArray(
-                            res.getIdentifier(answersName, "array", packageName)
-            ).asList()
-            data.add(
-                    Question(
-                            questions[i],
-                            answers,
-                            res.getDrawable(
-                                    res.getIdentifier(avatarName, "drawable", packageName), theme
-                            )
-                    )
-            )
-        }
-        return data
-    }
 }
